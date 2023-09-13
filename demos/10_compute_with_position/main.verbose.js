4c3b63061e1481a21daf1955b85e309c3d94578c
charlieroberts <charlie@charlie-roberts.com>
2023-09-13 02:07:06 -0400

    verbose js for demo 10

--- demos/10_compute_with_position/main.verbose.js
+++ demos/10_compute_with_position/main.verbose.js
@@ -1,71 +1,63 @@
 import { default as seagulls } from '../../seagulls.js'
 
-/* simplest_compute
+/* compute_with_position
  *
- * This demo combines a compute shader and a fragment shader.
- * The compute shader updates a single value, gradually
- * fading it between 0-1. The fragment shader uses that single
- * value to color every pixel.
- *
- * Compute shaders write to "storage" buffers. To make the
- * compute shader work, we need to:
- *
- * 1. Add a call to sg.buffers passing in an initial value
- * for our buffer (this must be a Float32Array).
- * 2. Add the buffer variable to both our compute shader
- * (for reading/writing) and our fragment shader (for reading only)
- * 3. Add a call to sg.compute() to schedule our compute shader
- * to run.
- * 4. We're not using the backbuffer in this example, so we'll
- * turn it off with sg.backbuffer( false ). Storage buffers must
- * be listed after the back buffer / sampler in our fragment shader;
- * but turning the back buffer off means we don't need to include
- * anything related to the back buffer in our shader.
- * 5. We need to specify a workgroup size. This helps determine
- * how many times per frame the compute shader will run. For our
- * purposes, we only want it to run once, so we'll use a workgroup
- * size of (1,1)
+ * This demo is very simple to demo 9, but
+ * instead of using the compute shader to calculate
+ * a single value we use it to calculate a
+ * different value for every pixel on the
+ * screen.
  */
 
 const render_shader = `${seagulls.constants.vertex}
 
-// the one f32 that we'll update in our compute shader
-// in the fragment shader we'll just read from it
-@group(0) @binding(0) var<storage> state: f32;
+// resolution of viewport
+@group(0) @binding(0) var<uniform> res:   vec2f;
+
+// declare state to be an array of 32-bit floats
+@group(0) @binding(1) var<storage> state: array<f32>;
 
 @fragment 
 fn fs( @builtin(position) pos : vec4f ) -> @location(0) vec4f {
-  let v = state;
+  // we derive the index into our array with the formula
+  // row number * width + column number
+  let idx : u32 = u32( pos.y * res.x + pos.x );
+  let v = state[ idx ];
   return vec4f( v,v,v, 1.);
 }`
 
 const compute_shader =`
-// in the compute shader we must mark that we can both
-// read from and write to our storage buffer.
-@group(0) @binding(0) var<storage, read_write> state:f32;
+@group(0) @binding(0) var<uniform> res:   vec2f;
 
-@compute
-// specify the workgroup size
-@workgroup_size(1,1)
+// in the compute shader, the storage buffer must be
+// declared as read_write, the default is read-only
+@group(0) @binding(1) var<storage, read_write> state: array<f32>;
+
+// here we specify a workgroup size of 64. this
+// should always be a power of 8 for optimization on
+// nvidia cards.
+@compute @workgroup_size(64,1)
 
 fn cs(@builtin(global_invocation_id) cell:vec3u)  {
-  // set value to be between 0-1
-  state = (state + .01) % 1.;
+  state[ cell.x ] = (state[ cell.x ] + .01) % 1.;
 }
 `
 
-const sg = await seagulls.init()
+const sg = await seagulls.init(),
+      size = window.innerWidth * window.innerHeight,
+      state = new Float32Array( size )
 
-// create a an array to hold our initial state
-const state = new Float32Array([ 0 ])
+// set state to random starting values
+for( let i = 0; i < size; i++ ) {
+  state[ i ] = Math.random()
+}
 
-// pass our array to the .buffers() method
+const workgroup_count = Math.ceil( size / 64 )  
+
 sg.buffers({ state })
-// turn the back buffer of
+  .uniforms({ resolution:[ window.innerWidth, window.innerHeight ] })
   .backbuffer( false )
-// schedule the compute shader to run
-  .compute( compute_shader )
-// schedule the fragment shader to run
+  .compute( compute_shader, [workgroup_count, 1, 1] )
   .render( render_shader )
-// start running
   .run()
+
