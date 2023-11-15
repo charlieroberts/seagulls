fd1905e6eadca5857fdf3a7f4c9ce55548e81826
charlieroberts <charlie@charlie-roberts.com>
2023-11-15 16:27:55 -0500

    update many particles demo

---
+++ demos/14_many_particles/compute.wgsl
@@ -0,0 +1,23 @@
+struct Particle {
+  pos: vec2f,
+  vel: vec2f
+};
+
+@group(0) @binding(0) var<uniform> res:   vec2f;
+@group(0) @binding(1) var<storage, read_write> state: array<Particle>;
+
+fn cellindex( cell:vec3u ) -> u32 {
+  let size = 8u;
+  return cell.x + (cell.y * size) + (cell.z * size * size);
+}
+
+@compute
+@workgroup_size(8,8)
+
+fn cs(@builtin(global_invocation_id) cell:vec3u)  {
+  let i = cellindex( cell );
+  let p = state[ i ];
+  var next = p.pos + (2. / res) * p.vel;
+  if( next.x >= 1. ) { next.x -= 2.; }
+  state[i].pos = next;
+}
--- demos/14_many_particles/main.js
+++ demos/14_many_particles/main.js
@@ -1,15 +1,10 @@
 import { default as seagulls } from '../../seagulls.js'
 
-const WORKGROUP_SIZE = 8
-
-let frame = 0
-
 const sg = await seagulls.init(),
-      render_shader  = await seagulls.import( './render.glsl' ),
-      compute_shader = await seagulls.import( './compute.glsl' )
+      render_shader  = await seagulls.import( './render.wgsl' ),
+      compute_shader = await seagulls.import( './compute.wgsl' )
 
 const NUM_PARTICLES = 1024, 
-      // must be evenly divisble by 4 to use wgsl structs
       NUM_PROPERTIES = 4, 
       state = new Float32Array( NUM_PARTICLES * NUM_PROPERTIES )
 
